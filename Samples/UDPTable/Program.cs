690c781ff9982a6248073138e3cfda6e0e87ac2f
Mohammadreza Taesiri <Taesiri@live.com>
2012-10-01 17:08:37 +0330

    UDPTable
    Sample Project Added : UDPTable

---
+++ Samples/UDPTable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using IPHelper;
+
+namespace UDPTable
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintData();
+            Console.ReadLine();
+        }
+
+        private static void PrintData()
+        {
+            UdpTable returnData = Functions.GetExtendedUdpTable(true);
+
+            Console.WriteLine("+============================================+");
+
+            Console.Write("| ");
+            Console.Write(String.Format("{0,-15}{1,-18}{2,-10}", "PID", "Local Address", "Port"));
+            Console.WriteLine("|");
+
+            Console.WriteLine("+============================================+");
+            foreach (UdpRow data in returnData)
+            {
+                if (data.LocalEndPoint != null)
+                {
+                    Console.Write("| ");
+
+                    Console.Write(String.Format("{0,-15}{1,-18}{2,-10}", data.ProcessId,
+                                                data.LocalEndPoint.Address,
+                                                data.LocalEndPoint.Port
+                                      ));
+
+
+                    Console.WriteLine("|");
+                }
+            }
+            Console.WriteLine("+============================================+");
+        }
+    }
+}
